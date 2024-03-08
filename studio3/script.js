927155cf16323d2dc0afd9ac29a5c96f5ce05cc0
yzabel <yzabelmartinez2@gmail.com>
2024-03-08 15:30:38 -0800

    fixed errors to studio 3

--- studio3/script.js
+++ studio3/script.js
@@ -2,7 +2,9 @@
 
     'use strict';
     console.log('reading js');
+ 
 
+    //INSTRUCTIONS OVERLAY
     document.querySelector('.close').addEventListener('click', function(event){
         event.preventDefault();
         document.querySelector('#overlay').className="hidden";
@@ -14,13 +16,14 @@
         }
     })
 
-
+    //VARIABLES
     const startGame = document.querySelector('#startgame');
     const gameControl = document.querySelector('#gamecontrol');
     const game = document.querySelector('#game');
     const score = document.querySelector('#score');
     const actionArea = document.querySelector('#actions');
 
+    //GAME IMAGES AND DATA
     const gameData = {
         dice: ['images/1fire.PNG', 'images/2ball.PNG', 'images/3ball.PNG', 'images/4ball.PNG', 'images/5ball.PNG', 'images/6ball.PNG'],
         players: ['player 1', 'player 2'],
@@ -32,38 +35,21 @@
         gameEnd: 29
     };
     
-    // // This gets the current player: 
-    // gameData.players[gameData.index]
-
-    // //This gets the first die and the second die: 
-    // gameData.dice[gameData.roll1-1]
-    // gameData.dice[gameData.roll2-1]
-
-    // //This gets the score of the current player: 
-    // gameData.score[gameData.index]
-
-    // //This gets the index, or turn
-    // gameData.index
-
-    // //This gets the individual dice values and the added dice value
-    // gameData.roll1
-    // gameData.roll2
-    // gameData.rollSum
-
-    // //This gets the winning threshold
-    // gameData.rollSum
-
+    //START THE GAME
     startGame.addEventListener("click",function(){
         //randomly set game index here
         gameData.index = Math.round(Math.random());
         console.log(gameData.index);
         
+        //GAME STARTED AND QUITTING OPTION
         gameControl.innerHTML='<h2>The Game Has Started!</h2>'
         score.innerHTML+='<button id="quit">Wanna Quit?</button>';
 
         document.getElementById('quit').addEventListener("click",function(){
             location.reload();
         });
+
+        //SET UP TURN AND THROWING DICE 
         function setUpTurn(){
             game.innerHTML = `<h3>It's ${gameData.players[gameData.index]}'s turn to roll the ball</h3>`;
             actionArea.innerHTML ='<button id="roll">Roll the Ball</button>';
@@ -111,10 +97,13 @@
                         
                     }
                 }
+
+                //SHOWING SCORES
                 function showCurrentScore(){
                     score.innerHTML = `<p><strong>${gameData.players[0]}</strong> currently has <strong>${gameData.score[0]}</strong> points <br> <strong>${gameData.players[1]}</strong> has <strong>${gameData.score[1]}</strong> points!</p>`;    
                 }
 
+                //WINNER AND START NEW GAME
                 function checkWinningCondition(){
                     if(gameData.score[gameData.index] > gameData.gameEnd){
                         score.innerHTML = `<h2>${gameData.players[gameData.index]}
